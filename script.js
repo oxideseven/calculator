a9543c0ef1cac074b9645b1e821c15a259e19c57
Aemilia <oxideseven@users.noreply.github.com>
2022-01-13 15:20:45 -0600

    Feature: decimals and backspace; Style: color

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ const btnMul = document.getElementById('btnMul');
 const btnDiv = document.getElementById('btnDiv');
 const btnEqu = document.getElementById('btnEqu');
 const btnClr = document.getElementById('btnClr');
+const btnDel = document.getElementById('btnDel');
+const btnDec = document.getElementById('btnDec');
 const displayOutput = document.getElementById('displayOutput');
 const resultOutput = document.getElementById('resultOutput');
 const buttons = document.querySelectorAll('button');
@@ -29,17 +31,49 @@ buttons.forEach((button) => {
 });
 
 function btnClicked(classClicked, contentClicked){
+    if (classClicked == 'clrBtn') {
+        reset();
+    }
+
+    if (classClicked == 'delBtn') {
+        if (num2Selected == true) {
+            num2 = num2.substring(0, num2.length - 1);
+            if (num2.indexOf('.') == -1) {
+                dec2Pressed = false;
+            }
+        }
+        if ((num2Selected == false) && (opSelected == false)) {
+            num1 = num1.substring(0, num1.length - 1);
+            if (num1.indexOf('.') == -1) {
+                dec1Pressed = false;
+            }
+        }
+    }
+
+    if (classClicked == 'decBtn') {
+        if ((opSelected == true) && (dec2Pressed == false)) {
+            num2 += contentClicked;
+            dec2Pressed = true;
+        }
+        if ((opSelected == false) && (dec1Pressed == false) && equalPressed == false) {
+            num1 += contentClicked;
+            dec1Pressed = true;
+        }
+    }
+    
+    if ((classClicked == 'equBtn') && (num2Selected == true)) {
+        equalPressed = true;
+        operate(num1, op, num2);
+        opSelected = false;
+        num2Selected = false;
+        dec1Pressed = false;
+        dec2Pressed = false;
+        
+    }
+
     if (equalPressed == false) {
-        if (classClicked == 'clrBtn') {
-            reset();
-        }
-        if (classClicked == 'equBtn') {
-            equalPressed = true;
-            operate(num1, op, num2);
-            opSelected = false;
-            num2Selected = false;
-        }
-        if (classClicked == 'opBtn'){
+
+        if (classClicked == 'opBtn') {
             if (num2Selected == false) {
                 op = contentClicked;
                 opSelected = true;
@@ -48,6 +82,10 @@ function btnClicked(classClicked, contentClicked){
                 equalPressed = true;
                 operate(num1, op, num2);
                 num2Selected = false;
+                op = contentClicked;
+                opSelected = true;
+                dec1Pressed = false;
+                dec2Pressed = false;
             }
         }
         if (classClicked == 'numBtn') {
@@ -58,10 +96,12 @@ function btnClicked(classClicked, contentClicked){
             else {
                 num1 += contentClicked;
             }
-        } 
+        }
+        
     }
     
     if (equalPressed == true) {
+
         if (classClicked == 'opBtn') {
             if (num2Selected == false) {
                 op = contentClicked;
@@ -70,28 +110,20 @@ function btnClicked(classClicked, contentClicked){
                 num2 = '';
             }
             if (num2Selected == true) {
-                equalPressed = true;
                 operate(num1, op, num2);
+                op = contentClicked;
+                opSelected = true;
                 num2Selected = false;
                 num1 = result;
                 num2 = '';
+                dec1Pressed = false;
+                dec2Pressed = false;
             }
         }
         if ((classClicked == 'numBtn') && (opSelected == true)) {
             num2 += contentClicked;
             num2Selected = true;
         }
-        if (classClicked == 'equBtn') {
-            operate(num1, op, num2);
-            opSelected = false;
-            num2Selected = false;
-        }
-        if (classClicked == 'clrBtn') {
-            reset();
-        }
-    }
-    if (classClicked == 'clrBtn') {
-        reset();
     }
     updateDisplay();
 }
@@ -107,6 +139,8 @@ function reset() {
     opSelected = false;
     equalPressed = false;
     num2Selected = false;
+    dec1Pressed = false;
+    dec2Pressed = false;
     initialize();
 }
 
@@ -114,6 +148,8 @@ function reset() {
 let opSelected = false;
 let equalPressed = false;
 let num2Selected = false;
+let dec1Pressed = false;
+let dec2Pressed = false;
 let num1 = '';
 let num2 = '';
 let op = '';
@@ -122,22 +158,22 @@ let result;
 //Functions
 
 function addition(num1, num2) {
-    result = parseInt(num1) + parseInt(num2);
+    result = parseFloat(num1) + parseFloat(num2);
     resultOutput.textContent = result;
 }
 
 function subtraction(num1, num2) {
-    result = parseInt(num1) - parseInt(num2);
+    result = parseFloat(num1) - parseFloat(num2);
     resultOutput.textContent = result;
 }
 
 function multiplication(num1, num2) {
-    result = parseInt(num1) * parseInt(num2);
+    result = parseFloat(num1) * parseFloat(num2);
     resultOutput.textContent = result;
 }
 
 function division(num1, num2) {
-    result = parseInt(num1) / parseInt(num2);
+    result = parseFloat(num1) / parseFloat(num2);
     resultOutput.textContent = result;
 }
 
