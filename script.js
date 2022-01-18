347506859b7522f82dab5622c01db823b91e78fd
Aemilia <oxideseven@users.noreply.github.com>
2022-01-18 09:51:46 -0600

    Refactor: clean up code

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const displayOutput = document.getElementById('displayOutput');
 const resultOutput = document.getElementById('resultOutput');
 const buttons = document.querySelectorAll('button');
 
-//Keyboard support
+//Keyboard Support
 
 document.addEventListener('keyup', (event) => {
     const operators = {
@@ -31,7 +31,7 @@ document.addEventListener('keyup', (event) => {
     }
 });
 
-//UI Functions
+//Mouse Support
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -39,59 +39,57 @@ buttons.forEach((button) => {
     });
 });
 
+//Function Variables
+
+let opSelected = false;
+let equalPressed = false;
+let num2Selected = false;
+let dec1Pressed = false;
+let dec2Pressed = false;
+let num1 = '';
+let num2 = '';
+let op = '';
+let result;
+
+//Functions
+
 function btnClicked(classClicked, contentClicked){
-    
-    if (classClicked == 'clrBtn') {
-        reset();
-    }
-
-    if (classClicked == 'delBtn') {
-        if (num2Selected == true) {
-            num2 = num2.substring(0, num2.length - 1);
-            if (num2.indexOf('.') == -1) {
-                dec2Pressed = false;
+    switch (classClicked) {
+        
+        case 'clrBtn':
+            reset();
+            break;
+        
+        case 'delBtn':
+            if (num2Selected == true) {
+                num2 = num2.substring(0, num2.length - 1);
+                if (num2.indexOf('.') == -1) {
+                    dec2Pressed = false;
+                }
+                if (num2 == '') {
+                    num2Selected = false;
+                }
             }
-        }
-        if ((num2Selected == false) && (opSelected == false) && (equalPressed == false)) {
-            num1 = num1.substring(0, num1.length - 1);
-            if (num1.indexOf('.') == -1) {
-                dec1Pressed = false;
+            if ((num2Selected == false) && (opSelected == false) ) {
+                num1 = num1.substring(0, num1.length - 1);
+                if (num1.indexOf('.') == -1) {
+                    dec1Pressed = false;
+                }
             }
-        }
-    }
-
-    if (classClicked == 'decBtn') {
-        if ((opSelected == true) && (dec2Pressed == false)) {
-            num2 += contentClicked;
-            dec2Pressed = true;
-        }
-        if ((opSelected == false) && (dec1Pressed == false) && equalPressed == false) {
-            num1 += contentClicked;
-            dec1Pressed = true;
-        }
-    }
-    
-    if ((classClicked == 'equBtn') && (num2Selected == true)) {
-        if ((num2 == 0) && (op == '÷')) {
-            resultOutput.textContent = 'nonononono!';
-        }
-        else {
-            equalPressed = true;
-            operate(num1, op, num2);
-            opSelected = false;
-            num2Selected = false;
-            dec1Pressed = false;
-            dec2Pressed = false; 
-        }
-    }
-
-    if (equalPressed == false) {
-
-        if (classClicked == 'opBtn') {
-            if (num2Selected == false) {
-                op = contentClicked;
-                opSelected = true;
+            break;
+        
+        case 'decBtn':
+            if ((opSelected == true) && (dec2Pressed == false)) {
+                num2 += contentClicked;
+                dec2Pressed = true;
             }
+            if ((opSelected == false) && (dec1Pressed == false) && equalPressed == false) {
+                num1 += contentClicked;
+                dec1Pressed = true;
+            }
+            break;
+        
+        case 'equBtn':
             if (num2Selected == true) {
                 if ((num2 == 0) && (op == '÷')) {
                     resultOutput.textContent = 'nonononono!';
@@ -100,60 +98,69 @@ function btnClicked(classClicked, contentClicked){
                     equalPressed = true;
                     operate(num1, op, num2);
                     num2Selected = false;
-                    op = contentClicked;
-                    opSelected = true;
-                    dec1Pressed = false;
-                    dec2Pressed = false;
+                    opSelected = false; 
                 }
             }
-            
-        }
-        if (classClicked == 'numBtn') {
-            if (opSelected == true) {
+            break;
+        
+        case 'opBtn':
+            if (equalPressed == false) {
+                if (num2Selected == false) {
+                    op = contentClicked;
+                    opSelected = true;
+                }
+                if (num2Selected == true) {
+                    if ((num2 == 0) && (op == '÷')) {
+                        resultOutput.textContent = 'nonononono!';
+                    }
+                    else {
+                        equalPressed = true;
+                        operate(num1, op, num2);
+                        num2Selected = false;
+                        op = contentClicked;
+                        opSelected = true;
+                    }
+                }
+            }
+            if (equalPressed == true) {
+                if (num2Selected == false) {
+                    op = contentClicked;
+                    opSelected = true;
+                    num1 = result;
+                    num2 = '';
+                }
+                if (num2Selected == true) {
+                    if ((num2 == 0) && (op == '÷')) {
+                        resultOutput.textContent = 'nonononono!';
+                    }
+                    else {
+                        operate(num1, op, num2);
+                        op = contentClicked;
+                        num2Selected = false;
+                        num1 = result;
+                        num2 = '';
+                        opSelected = true;
+                    }
+                }
+            }
+            break;
+        
+        case 'numBtn':
+            if (equalPressed == false) {
+                if (opSelected == true) {
+                    num2 += contentClicked;
+                    num2Selected = true;
+                }
+                else {
+                    num1 += contentClicked;
+                }
+            }
+            if ((equalPressed == true) && (opSelected == true)) {
                 num2 += contentClicked;
                 num2Selected = true;
             }
-            else {
-                num1 += contentClicked;
-            }
-        }
-        
+            break;
     }
-    
-    if (equalPressed == true) {
-
-        if (classClicked == 'opBtn') {
-            if (num2Selected == false) {
-                op = contentClicked;
-                opSelected = true;
-                num1 = result;
-                num2 = '';
-            }
-            if (num2Selected == true) {
-                if ((num2 == 0) && (op == '÷')) {
-                    resultOutput.textContent = 'nonononono!';
-                }
-                else {
-                    operate(num1, op, num2);
-                    op = contentClicked;
-                    opSelected = true;
-                    num2Selected = false;
-                    num1 = result;
-                    num2 = '';
-                    dec1Pressed = false;
-                    dec2Pressed = false;
-                }
-            }
-        }
-        if ((classClicked == 'numBtn') && (opSelected == true)) {
-            num2 += contentClicked;
-            num2Selected = true;
-        }
-    }
-    updateDisplay();
-}
-
-function updateDisplay() {
     displayOutput.textContent = `${num1} ${op} ${num2}`;
 }
 
@@ -169,18 +176,7 @@ function reset() {
     initialize();
 }
 
-//Function Variables
-let opSelected = false;
-let equalPressed = false;
-let num2Selected = false;
-let dec1Pressed = false;
-let dec2Pressed = false;
-let num1 = '';
-let num2 = '';
-let op = '';
-let result;
-
-//Functions
+//Calc Functions
 
 function addition(num1, num2) {
     result = parseFloat(num1) + parseFloat(num2);
@@ -202,6 +198,11 @@ function division(num1, num2) {
     cleanUP(result);
 }
 
+function cleanUP (result){
+    result = result = Math.round((result + Number.EPSILON) * 1000000) / 1000000;
+    resultOutput.textContent = result;
+}
+
 function operate(num1, op, num2) {
 
     switch (op) {
@@ -218,12 +219,8 @@ function operate(num1, op, num2) {
             return division(num1, num2);
             break;
     }
-    
-}
-
-function cleanUP (result){
-    result = result = Math.round((result + Number.EPSILON) * 1000000) / 1000000;
-    resultOutput.textContent = result;
+    dec1Pressed = false;
+    dec2Pressed = false;
 }
 
 //Initialization
